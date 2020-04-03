1fd0490e76dc6ace416a8e23af6f8490bcbfc4bc
yamp <coramdeo0708@gmail.com>
2020-04-03 12:41:58 +0900

    [DP] upload 1010 solution.

---
+++ dynamic_programming/1010-다리놓기.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+// 1010 - 다리놓기
+int N, M;
+int cache[31][31];
+
+int putBridge(int n, int m) {
+	if (n == 1) return m; 
+
+	int &ret = cache[n][m];
+	if (ret != -1) {
+	//	printf("repeated case!\n");
+		return ret;
+	}
+
+	ret = 0;
+	for (int i = 0; i <= m - n; ++i) {
+		ret += putBridge(n - 1, m - (i + 1));
+	}
+
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+	for (int t = 0; t < T; ++t) {
+		cin >> N >> M;
+		memset(cache, -1, sizeof(cache));
+		cout << putBridge(N, M) << "\n";	
+	}
+
+	return 0;
+}
