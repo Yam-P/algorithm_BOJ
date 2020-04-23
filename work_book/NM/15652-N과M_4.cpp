6ae5af4dc2ee47a71a93a364996c6736d56abf8d
yamp <coramdeo0708@gmail.com>
2020-04-23 11:21:54 +0900

    [NM] upload 15652 solution.

---
+++ work_book/NM/15652-N과M_4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 15650 - N과 M (4)
+	
+int N, M;
+
+void solve(vector<int> seq, int idx) {
+	if (seq.size() == M) {
+		for (int i = 0; i < seq.size(); ++i) {
+			cout << seq[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int next = idx; next < N; ++next) {
+		seq.push_back(next + 1);
+		solve(seq, next);
+		seq.pop_back();
+	} 
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> N >> M;
+
+	vector<int> _seq;
+	solve(_seq, 0);
+
+	return 0;
+}
